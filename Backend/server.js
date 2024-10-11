fbbca6004ccc4d83d6261c46a33d871019d77bc9
Praharsh-T <ppraharsht@gmail.com>
2024-10-11 14:17:05 +0530

    added c

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,14 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://agri-connect-ym23.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
