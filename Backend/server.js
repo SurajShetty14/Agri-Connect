13526b4473d54164177a786dcc397b901367736a
Praharsh-T <ppraharsht@gmail.com>
2024-10-16 12:55:21 +0530

    org

--- Backend/server.js
+++ Backend/server.js
@@ -10,13 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-app.use(
-  cors({
-    origin: "http://localhost:5173/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Routes
